e1ce7123600281e0da356f4bd6e241885e6e11cd
samanfatima15 <samanfatima768@gmail.com>
2026-07-30 12:26:09 +0500

    Update persivialeetcode.py

--- persivialeetcode.py
+++ persivialeetcode.py
@@ -4,3 +4,14 @@ def twosum(nums,target):
         for j in range(i+1,len(nums)):
             if nums[i]+nums[j]==target:
                 return[i,j]
+                
+#PALINDROME NUM QUESTION
+def palindrome(numgiven):
+    numberstring=str(numgiven)
+    reversed= numberstring[::-1] #start from back n all the way to first number 
+    if numberstring==reversed:
+        print("Number is palindrome")
+        return True
+    else:
+        print("Number is NOT palindome")
+        return False
